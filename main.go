a17c300a658fd378faffa0f2e5d6bace3288bec9
vamsijavvadi7 <59219320+vamsijavvadi7@users.noreply.github.com>
2021-12-28 21:01:57 +0530

    Update main.go

--- main.go
+++ main.go
@@ -13,13 +13,19 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 )
-
+	type User struct {
+		Mail string // <-- CHANGED THIS LINE
+		Password string
+		Id   string // <-- CHANGED THIS LINE
+		Role string
+	}
 func main() {
 
 	r := mux.NewRouter()
 	godotenv.Load()
 	port := os.Getenv("PORT")
-	r.HandleFunc("/{mail}", Fetch).Methods("GET")
+	r.HandleFunc("/login/email/{mail}", Fetch).Methods("GET")
+  r.HandleFunc("/login/{email}/{password}",loginCheck).Methods("GET")
 	// if there is an error opening the connection, handle it
 
 	// defer the close till after the main function has finished
@@ -39,15 +45,52 @@ func main() {
 	//   defer insert.Close()
 }
 
+func loginCheck(w http.ResponseWriter, r *http.Request){
+w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // Gets params
+  
+db, err := sql.Open("mysql", "b43dbfed48dc1d:395f6a59@tcp(us-cdbr-east-05.cleardb.net)/heroku_ae8d9f2c5bc1ed0")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	rows, err := db.Query("call getstudents()")
+	if err != nil {
+
+		panic(err.Error())
+
+	}
+	defer db.Close()
+	users := make([]*User, 0)
+	for rows.Next() {
+		user := new(User)
+		err := rows.Scan(&user.Mail, &user.Password, &user.Id, &user.Role)
+		if err != nil {
+			panic(err)
+		}
+		users = append(users, user)
+	}
+	defer rows.Close()
+	type Result struct{
+		Status string `json:"status"`
+		Role string `json:"role"`
+	}
+	res:=Result{Status:"False",Role:""}
+	for _, item := range users {
+		if item.Mail== params["email"]  && item.Password==params["password"]{
+			res.Role=item.Role
+			res.Status="True"
+			json.NewEncoder(w).Encode(res)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(res)
+
+}
 func Fetch(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	type User struct {
-		Mail string // <-- CHANGED THIS LINE
-		Pass string
-		Id   string // <-- CHANGED THIS LINE
-		Role string
-	}
+
 	db, err := sql.Open("mysql", "b43dbfed48dc1d:395f6a59@tcp(us-cdbr-east-05.cleardb.net)/heroku_ae8d9f2c5bc1ed0")
 	if err != nil {
 		panic(err.Error())
@@ -63,7 +106,7 @@ func Fetch(w http.ResponseWriter, r *http.Request) {
 	users := make([]*User, 0)
 	for rows.Next() {
 		user := new(User)
-		err := rows.Scan(&user.Mail, &user.Pass, &user.Id, &user.Role)
+		err := rows.Scan(&user.Mail, &user.Password, &user.Id, &user.Role)
 		if err != nil {
 			panic(err)
 		}
